160f09c9e30f7e0cb4079d42820f33b8bdcd1aa7
Andrew Brown <andrew@monsterboxpro.com>
2021-01-26 21:30:00 -0500

    Add debugging

--- lib/teacherseat_permissions/model/permissible.rb
+++ lib/teacherseat_permissions/model/permissible.rb
@@ -8,7 +8,9 @@ module TeacherseatPermissions
       end
 
       def permission? permission, conditions={}
+        logger.debug "permission?: #{permission} #{conditions.to_json}"
         provider, namespace, system, subsystem, action = permission.downcase.split(':')
+        logger.debug "permission?: provider: #{provider} namespace: #{namespace} system: #{system} subsystem: #{subsystem} action: #{action}"
 
         result = self.permissions_tree
           .try('[]',provider)
@@ -16,29 +18,38 @@ module TeacherseatPermissions
           .try('[]',system)
           .try('[]',subsystem)
           .try('[]',action)
+        logger.debug("result: #{result.inspect}")
 
         if result
           # if there are conditions process them
           if result['condition'] && conditions != :ignore
+            logger.debug("checking condition")
             conditions.stringify_keys!
             result['effect'] == 'allow' &&
             result['condition'].all?{ |k,v|
               if conditions.key?(k)
                 if v.is_a?(Array)
+                  logger.debug("condition key array")
                   v.include?(conditions[k])
+                elsif conditions[k].to_s == v.to_s
+                  logger.debug("condition string array")
+                  true
                 elsif
-                  conditions[k] == v
-                else
+                  logger.debug("condition not match : return false")
                   false
                 end
               else
+                logger.debug("no condition key: return false")
                 false
               end
             }
           else
+            logger.debug("no condition to check")
+            logger.debug("return: #{result['effect'] == 'allow'}")
             result['effect'] == 'allow'
           end
         else
+            logger.debug("return false")
           false
         end
       end
